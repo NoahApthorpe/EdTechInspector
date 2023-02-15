b38de792ca88a6478868a4991c1bd4f161f1c458
andrekabir <andrekabir@gmail.com>
2023-02-14 18:24:33 -0600

    testing separate section

--- service-worker-utils.js
+++ service-worker-utils.js
@@ -476,7 +476,17 @@ function checkRequest(request, searchTerms, tdsResult, timeStamp, requestBaseDom
   // }
 
 
-  ;
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "http://127.0.0.1:5000/save");
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onload = () => console.log(xhr.responseText);
+  xhr.send(JSON.stringify( {
+    // 'UrlLeak' : url_leak_send,
+    // 'PostLeak' : post_leak_send
+    'UrlLeak' : "hello",
+    'PostLeak' : "world"
+  }))
 
 // if (posted == false) {
 //   posted = true;
@@ -498,41 +508,41 @@ function checkRequest(request, searchTerms, tdsResult, timeStamp, requestBaseDom
 // }
 
 
-  (async function f() {
-    const res = await fetch('http://127.0.0.1:5000/save', {
-      headers : {
-          'Content-Type' : 'application/json'
-      },
-      method : 'POST',
-      body : JSON.stringify( {
-          'UrlLeak' : url_leak_send,
-          'PostLeak' : post_leak_send
-      })
-    //   body : JSON.stringify( {
-    //     'UrlLeak' : 'test3',
-    //     'PostLeak' : 'test2'
-    // })
-    })
-    .then(function (response){
+  // (async function f() {
+  //   const res = await fetch('http://127.0.0.1:5000/save', {
+  //     headers : {
+  //         'Content-Type' : 'application/json'
+  //     },
+  //     method : 'POST',
+  //     body : JSON.stringify( {
+  //         'UrlLeak' : url_leak_send,
+  //         'PostLeak' : post_leak_send
+  //     })
+  //   //   body : JSON.stringify( {
+  //   //     'UrlLeak' : 'test3',
+  //   //     'PostLeak' : 'test2'
+  //   // })
+  //   })
+  //   .then(function (response){
   
-        if(response.ok) {
-            response.json()
-            .then(function(response) {
-                console.log(response);
-            });
-        }
-        else {
-            throw Error('Something went wrong');
-        }
-    })
-    .catch(function(error) {
-        console.log(error);
-    });
+  //       if(response.ok) {
+  //           response.json()
+  //           .then(function(response) {
+  //               console.log(response);
+  //           });
+  //       }
+  //       else {
+  //           throw Error('Something went wrong');
+  //       }
+  //   })
+  //   .catch(function(error) {
+  //       console.log(error);
+  //   });
 
-    const json = await res.json();
-    console.log(json);
-    console.log("Hello!");
-  })();
+  //   const json = await res.json();
+  //   console.log(json);
+  //   console.log("Hello!");
+  // })();
   
   
 
